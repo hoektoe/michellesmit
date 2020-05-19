b65493c4146b5c6e16e95e5b146457f747292726
hoektoe <hoektoe@gmail.com>
2020-05-19 15:04:47 +0200

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,7 +14,7 @@ export default {
     meta: [
       { charset: 'utf-8' },
       {'http-equiv': 'X-UA-Compatible', content: 'IE=edge'},
-      { name: "viewport", content: "width=device-width, initial-scale=1, maximum-scale=1" },
+      { name: "viewport", content: "width=device-width, initial-scale=1, maximum-scale=5" },
       { hid: 'description', name: 'description', content: 'My passion is within the field of addiction care. I have worked within multidisciplinary teams at in-patient facilities caring for patients that have both substance and process addiction. These addictions range from; alcohol, prescription tablets, cocaine, heroin, methamphetamines to shopping, gaming, and gambling to name a few. In addition, many of the patients I have treated over the years have a dual diagnosis. Often, addiction is comorbid with a psychiatric diagnosis such as depression, anxiety, PTSD, personality disorders or mood disorders.' },
       { name: "keywords", hid: "keywords", content: "Michelle Smit, Addiction Therapy, Psychologist, Cape Town" },
       { name: "theme-color", content: "#ccffd9" },
@@ -32,7 +32,14 @@ export default {
       { name: "google-site-verification", content: "AzlQXUEUdzWUExirM6sDAXRkOT2Rnqnxz42cbjdXHc8"}
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: "preconnect", href:"https://www.google.co.za" },
+      { rel: "preconnect", href:"https://www.google.com" },
+      { rel: "preconnect", href:"https://www.google-analytics.com" },
+      { rel: "preconnect", href:"https://googleads.g.doubleclick.net" },
+      { rel: "preconnect", href:"https://www.googleadservices.com" },
+      { rel: "preconnect", href:"https://www.googletagmanager.com" },
+      { rel: "canonical", href:"https://www.michellesmit.com" },
     ]
   },
   /*
