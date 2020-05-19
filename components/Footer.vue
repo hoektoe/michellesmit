6ed533d68eb841f2c799cd6609cec86d6f5cd2c2
hoektoe <hoektoe@gmail.com>
2020-05-19 12:30:14 +0200

    Lightgallery Improvements

--- components/Footer.vue
+++ components/Footer.vue
@@ -3,7 +3,7 @@
     <div class="container mx-auto px-4">
       <div class="flex flex-wrap">
         <div class="w-full lg:w-2/12 px-4">
-          <img class="object cover footer-logo" src="../assets/img/logo_block.svg"/>
+          <img alt="Michelle Smit Logo Artum Lilly" class="object cover footer-logo" src="../assets/img/logo_block.svg"/>
         </div>
         <div class="w-full lg:w-6/12 px-4">
           <h4 class="text-3xl text-green-200 font-semibold uppercase">Michelle Smit</h4>
@@ -23,15 +23,15 @@
               <span class="block uppercase text-gray-400 text-sm font-semibold mb-2">Sitemap</span>
               <ul class="list-unstyled">
                 <li>
-                  <a class="text-gray-600 hover:text-gray-900 font-semibold block pb-2 text-sm"
+                  <a class="text-gray-300 hover:text-gray-900 font-semibold block pb-2 text-sm"
                     href="#about">About</a>
                 </li>
                 <li>
-                  <a class="text-gray-600 hover:text-gray-900 font-semibold block pb-2 text-sm"
+                  <a class="text-gray-300 hover:text-gray-900 font-semibold block pb-2 text-sm"
                     href="#therapy">Therapy Services</a>
                 </li>
                 <li>
-                  <a class="text-gray-600 hover:text-gray-900 font-semibold block pb-2 text-sm"
+                  <a class="text-gray-300 hover:text-gray-900 font-semibold block pb-2 text-sm"
                     href="#publication">Publication</a>
                 </li>
               </ul>
@@ -42,7 +42,7 @@
       <hr class="my-6 border-gray-400" />
       <div class="flex flex-wrap items-center md:justify-between justify-center">
         <div class="w-full md:w-4/12 px-4 mx-auto text-center">
-          <div class="text-sm text-gray-600 font-semibold py-1">
+          <div class="text-sm text-gray-300 font-semibold py-1">
             Copyright © {{date}} Michelle Smit.
           </div>
         </div>
--- pages/index.vue
+++ pages/index.vue
@@ -187,43 +187,35 @@
                   <div class="flex-auto p-5 lg:p-10">
                     <h4 class="text-2xl font-semibold">Schedule your appointment</h4>
                     <div class="relative w-full mb-3 mt-8">
-                      <label
-                        class="block uppercase text-gray-700 text-xs font-bold mb-2"
-                        for="full-name"
-                        >Full Name</label
-                      ><input
+                      <label class="block uppercase text-gray-700 text-xs font-bold mb-2" for="full_name">Full Name</label>
+                      <input
                         type="text"
                         class="px-3 py-3 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow focus:outline-none focus:shadow-outline w-full"
                         placeholder="Full Name"
                         name="full_name"
+                        id="full_name"
                         style="transition: all 0.15s ease 0s;"
                       />
                     </div>
                     <div class="relative w-full mb-3">
-                      <label
-                        class="block uppercase text-gray-700 text-xs font-bold mb-2"
-                        for="email"
-                        >Email</label
-                      ><input
-                        type="email"
+                      <label class="block uppercase text-gray-700 text-xs font-bold mb-2" for="email">Email</label>
+                      <input type="email"
                         class="px-3 py-3 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow focus:outline-none focus:shadow-outline w-full"
                         placeholder="Email"
                         name="email"
+                        id="email"
                         style="transition: all 0.15s ease 0s;"
                       />
                     </div>
                     <div class="relative w-full mb-3">
-                      <label
-                        class="block uppercase text-gray-700 text-xs font-bold mb-2"
-                        for="message"
-                        >Message</label
-                      ><textarea
+                      <label  class="block uppercase text-gray-700 text-xs font-bold mb-2" for="message">Message</label>
+                      <textarea
                         rows="4"
                         cols="80"
                         name="message"
+                        id="message"
                         class="px-3 py-3 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow focus:outline-none focus:shadow-outline w-full"
-                        placeholder="Type a message..."
-                      ></textarea>
+                        placeholder="Type a message..."></textarea>
                     </div>
                     <div class="text-center mt-6">
                       <button
