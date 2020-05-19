88ca0a0f340e178da48ae27cb17f230a28d701ee
hoektoe <hoektoe@gmail.com>
2020-05-19 13:54:58 +0200

    Revert "Images move to piio"
    This reverts commit a3580028e34b8aca0554479b56721c6a1bbb075f.

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,7 +14,6 @@ export default {
     meta: [
       { charset: 'utf-8' },
       {'http-equiv': 'X-UA-Compatible', content: 'IE=edge'},
-      { name: "viewport", content: "width=device-width, initial-scale=1" },
       { hid: 'description', name: 'description', content: 'My passion is within the field of addiction care. I have worked within multidisciplinary teams at in-patient facilities caring for patients that have both substance and process addiction. These addictions range from; alcohol, prescription tablets, cocaine, heroin, methamphetamines to shopping, gaming, and gambling to name a few. In addition, many of the patients I have treated over the years have a dual diagnosis. Often, addiction is comorbid with a psychiatric diagnosis such as depression, anxiety, PTSD, personality disorders or mood disorders.' },
       { name: "keywords", hid: "keywords", content: "Michelle Smit, Addiction Therapy, Psychologist, Cape Town" },
       { name: "theme-color", content: "#ccffd9" },
