24980c39f343c3d046ca6551be83ba5572c88cf3
Martin Smit <hoektoe@gmail.com>
2020-11-02 07:38:07 +0200

    Core installation nextjs sanity

--- plugins/vue-piio.js
+++
@@ -1,7 +0,0 @@
-(function(i,m,a,g,e) {
-    e = i.getElementsByTagName(m)[0], (g = i.createElement(m)).src = "//pcdn.piiojs.com/"+a+"/image.min.js",
-    g.onerror = function() {
-        (g = i.createElement(m)).src = "https://fs.piio.co/image-failover.min.js",
-        e.parentNode.insertBefore(g, e);
-    }, e.parentNode.insertBefore(g, e);
-}(document, "script", "nbljy1"));
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,26 +1,32 @@
-/*
-** TailwindCSS Configuration File
-**
-** Docs: https://tailwindcss.com/docs/configuration
-** Default: https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
-*/
-const defaultTheme = require('tailwindcss/defaultTheme')
-
 module.exports = {
-  purge: {
-    enabled: true,
-    content: ['./src/**/*.html', './pages/*.vue', './layouts/*.vue', './components/*.vue'],
-  },
   theme: {
     extend: {
-      fontFamily: {
-        mono: [
-          'Open Sans',
-          ...defaultTheme.fontFamily.mono,
-        ]
-      }
-    }
+      colors: {
+        'accent-1': '#FAFAFA',
+        'accent-2': '#EAEAEA',
+        'accent-7': '#333',
+        success: '#0070f3',
+        cyan: '#79FFE1',
+      },
+      spacing: {
+        28: '7rem',
+      },
+      letterSpacing: {
+        tighter: '-.04em',
+      },
+      lineHeight: {
+        tight: 1.2,
+      },
+      fontSize: {
+        '5xl': '2.5rem',
+        '6xl': '2.75rem',
+        '7xl': '4.5rem',
+        '8xl': '6.25rem',
+      },
+      boxShadow: {
+        small: '0 5px 10px rgba(0, 0, 0, 0.12)',
+        medium: '0 8px 30px rgba(0, 0, 0, 0.12)',
+      },
+    },
   },
-  variants: {},
-  plugins: []
 }
