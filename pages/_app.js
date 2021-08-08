a24ce2b5257590dfba12f801ed42f8c6b3784082
hoektoe <hoektoe@gmail.com>
2021-08-09 01:19:55 +0200

    From

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
-import '../styles/tailwind.css'
+import "../styles/tailwind.css";
+import { useEffect } from "react";
+import TagManager from "react-gtm-module";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  useEffect(() => {
+    TagManager.initialize({ gtmId: "GTM-PFK9LKH" });
+  }, []);
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
