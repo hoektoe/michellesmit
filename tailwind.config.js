dddbdc903d6cf490aa93474d9c2b7d19a574ec6b
hoektoe <hoektoe@gmail.com>
2021-08-15 11:55:18 +0200

    t

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,62 +1,61 @@
 /*
-** TailwindCSS Configuration File
-**
-** Docs: https://tailwindcss.com/docs/configuration
-** Default: https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
-*/
-
+ ** TailwindCSS Configuration File
+ **
+ ** Docs: https://tailwindcss.com/docs/configuration
+ ** Default: https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
+ */
 
 module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Roboto', 'sans-serif']
+        sans: ["Roboto", "sans-serif"],
       },
       colors: {
         brand: {
-          100: '#FFFFFF',
-          200: '#F8FBFB',
-          300: '#D8E9E9',
-          400: '#B7D7D7',
-          500: '#96C5C5',
-          600: '#75B3B3',
-          700: '#579E9E',
-          800: '#457D7D',
-          900: '#335C5C',
-          DEFAULT: '#96C5C5'
+          100: "#FFFFFF",
+          200: "#F8FBFB",
+          300: "#D8E9E9",
+          400: "#B7D7D7",
+          500: "#96C5C5",
+          600: "#75B3B3",
+          700: "#579E9E",
+          800: "#457D7D",
+          900: "#335C5C",
+          DEFAULT: "#96C5C5",
         },
         accent: {
-          100: '#A3C4DB',
-          200: '#7EACCD',
-          300: '#5A95BF',
-          400: '#407BA5',
-          500: '#326081',
-          600: '#24445C',
-          700: '#152937',
-          800: '#070E12',
-          900: '#000000',
-          DEFAULT: '#326081'
-        }
-      }
-    }
+          100: "#A3C4DB",
+          200: "#7EACCD",
+          300: "#5A95BF",
+          400: "#407BA5",
+          500: "#326081",
+          600: "#24445C",
+          700: "#152937",
+          800: "#070E12",
+          900: "#000000",
+          DEFAULT: "#326081",
+        },
+      },
+    },
   },
   variants: {},
   plugins: [
-    require('@tailwindcss/typography'),
-    require('@tailwindcss/aspect-ratio'),
-    require('@tailwindcss/forms')
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/aspect-ratio"),
+    require("@tailwindcss/forms"),
   ],
   purge: {
-    enabled: process.env.NODE_ENV === 'production',
+    enabled: process.env.NODE_ENV === "production",
     content: [
-      'components/**/*.vue',
-      'layouts/**/*.vue',
-      'pages/**/*.vue',
-      'plugins/**/*.js',
-      'nuxt.config.js',
+      "components/**/*.js",
+      "layouts/**/*.js",
+      "pages/**/*.js",
+      "plugins/**/*.js",
+      "next.config.js",
       // TypeScript
-      'plugins/**/*.ts',
-      'nuxt.config.ts'
-    ]
-  }
-}
+      "plugins/**/*.ts",
+      "next.config.ts",
+    ],
+  },
+};
