956a7e545cb50dbccce7943586db9f9442e525d3
hoektoe <hoektoe@gmail.com>
2021-08-15 13:08:58 +0200

    uid

--- components/Text.js
+++ components/Text.js
@@ -1,17 +1,15 @@
-import React from "react"
-import SbEditable from "storyblok-react"
-import { render } from "storyblok-rich-text-react-renderer"
+import React from "react";
+import SbEditable from "storyblok-react";
+import { render } from "storyblok-rich-text-react-renderer";
 
 const Text = ({ blok }) => {
   return (
     <SbEditable content={blok} key={blok._uid}>
-      <div className="bg-white w-full">
-        <div className="container mx-auto py-12">
-          {render(blok.text)}     
-        </div>
+      <div className="w-full bg-white">
+        <div className="container py-12 mx-auto">{render(blok.text)}</div>
       </div>
     </SbEditable>
-  )
-}
+  );
+};
 
-export default Text
+export default Text;
