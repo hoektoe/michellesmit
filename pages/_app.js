ad63fb562e3fbf20132b73f32ece2de916d31f94
hoektoe <hoektoe@gmail.com>
2021-09-27 16:46:12 +0200

    Updated GTM

--- pages/_app.js
+++ pages/_app.js
@@ -4,7 +4,7 @@ import TagManager from "react-gtm-module";
 
 function MyApp({ Component, pageProps }) {
   useEffect(() => {
-    TagManager.initialize({ gtmId: "GTM-PFK9LKH" });
+    TagManager.initialize({ gtmId: "GTM-M3VH9T3" });
   }, []);
   return <Component {...pageProps} />;
 }
