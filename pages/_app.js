405e716f3c24955fe0bf18a32cc2f0ff23176afc
hoektoe <hoektoe@gmail.com>
2022-06-26 16:02:20 +0200

    GA

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,27 @@
 import "../styles/tailwind.css";
 import { useEffect } from "react";
 import TagManager from "react-gtm-module";
+import Script from 'next/script'
 
 function MyApp({ Component, pageProps }) {
   useEffect(() => {
     TagManager.initialize({ gtmId: "GTM-PFTRW6W" });
   }, []);
-  return <Component {...pageProps} />;
+  return <>
+    <Script
+
+      strategy="afterInteractive"
+      src={`https://www.googletagmanager.com/gtag/js?id=G-CL0R5VTP3D`} />
+
+    <Script strategy="afterInteractive">
+      {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-CL0R5VTP3D');
+                `}
+    </Script><Component {...pageProps} />
+  </>;
 }
 
 export default MyApp;
