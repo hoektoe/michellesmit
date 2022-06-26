4bbf1dc34896eeb076bacecce6118edd89ad9f63
hoektoe <hoektoe@gmail.com>
2022-06-26 16:11:14 +0200

    Switch to basic analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,26 +1,40 @@
 import "../styles/tailwind.css";
 import { useEffect } from "react";
-import TagManager from "react-gtm-module";
 import Script from 'next/script'
+import { useRouter } from "next/router";
+import * as gtag from "../lib/gtag";
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
   useEffect(() => {
-    TagManager.initialize({ gtmId: "GTM-PFTRW6W" });
-  }, []);
+    const handleRouteChange = (url) => {
+      gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+
+  }, [router.events]);
   return <>
+    {/* Global Site Tag (gtag.js) - Google Analytics */}
     <Script
-
       strategy="afterInteractive"
-      src={`https://www.googletagmanager.com/gtag/js?id=G-CL0R5VTP3D`} />
-
-    <Script strategy="afterInteractive">
-      {`
+      src={`https://www.googletagmanager.com/gtag/js?id=G-CL0R5VTP3D`}
+    />
+    <Script
+      strategy="afterInteractive"
+      dangerouslySetInnerHTML={{
+        __html: `
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
-            gtag('config', 'G-CL0R5VTP3D');
-                `}
-    </Script><Component {...pageProps} />
+            gtag('config', 'G-CL0R5VTP3D', {
+              page_path: window.location.pathname,
+            });
+          `,
+      }}
+    /><Component {...pageProps} />
   </>;
 }
 
