ba3b66fb219c1664df3e64acc89dbf62f1899429
Martin Smit <hoektoe@gmail.com>
2024-09-07 17:24:46 +0200

    chore: Update meta tags for SEO optimization

--- components/Page.js
+++ components/Page.js
@@ -20,44 +20,24 @@ export default function Page({ blok }) {
           <meta
             name="og:title"
             property="og:title"
-            content={blok.seo.og_title ? blok.seo.og_title : blok.seo.title}
+            content={`The Counsellors Couch - Adam Labuschagne`}
           />
           <meta
             name="og:description"
             property="og:description"
-            content={
-              blok.seo.og_description
-                ? blok.seo.og_description
-                : blok.seo.description
-            }
+            content={`The Counsellor's Couch offers compassionate and professional mental health counseling services, providing a safe space for individuals to explore personal challenges, improve well-being, and achieve personal growth.`}
           />
           {/* <meta property="og:url" content={router.asPath} /> */}
           <meta
             name="twitter:title"
-            content={
-              blok.seo.twitter_title ? blok.seo.twitter_title : blok.seo.title
-            }
+            content={`The Counsellors Couch - Adam Labuschagne`}
           />
           <meta
             name="twitter:description"
-            content={
-              blok.seo.twitter_description
-                ? blok.seo.twitter_description
-                : blok.seo.description
-            }
+            content={`The Counsellor's Couch offers compassionate and professional mental health counseling services, providing a safe space for individuals to explore personal challenges, improve well-being, and achieve personal growth.`}
           />
-          <meta
-            property="og:image"
-            content={
-              blok.seo.og_image ? blok.seo.og_image : HOME_META_IMAGE_URL
-            }
-          />
-          <meta
-            name="twitter:image"
-            content={
-              blok.seo.og_image ? blok.seo.og_image : HOME_META_IMAGE_URL
-            }
-          />
+          <meta property="og:image" content={HOME_META_IMAGE_URL} />
+          <meta name="twitter:image" content={HOME_META_IMAGE_URL} />
         </Head>
 
         {/* {JSON.stringify(blok, null, "\t")} */}
